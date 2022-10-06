d2c99cf1b6c32a70ab3094936ecdf3bb5af22c69
Trevor Pfizenmaier <elektrikspark@gmail.com>
2022-10-06 03:31:40 -0500

    fix: replaced date-fns with moment

--- components/date.tsx
+++ components/date.tsx
@@ -1,16 +1,7 @@
-import { format, parseISO } from 'date-fns';
-import { useEffect, useState } from 'react';
+import Moment from 'moment';
 
-// Sanity Date is an ISO 8601 formatted string, so we use toString() to get the correct type for date-fns
 export default function Date({ dateCreated }: { dateCreated: Date }) {
-  const [date, setDate] = useState(parseISO(dateCreated.toString()));
-
-  useEffect(() => {
-    // prevent server side rendering
-    if (dateCreated && window) {
-      setDate(parseISO(dateCreated.toString()));
-    }
-  }, [dateCreated]);
-
-  return <time dateTime={dateCreated.toString()}>{format(date, 'LLLL d, yyyy')}</time>;
+  return (
+    <time dateTime={dateCreated.toString()}>{Moment(dateCreated).format('MMMM D, YYYY')}</time>
+  );
 }
