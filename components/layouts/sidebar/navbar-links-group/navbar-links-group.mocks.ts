d8096920d3acf760bea8b753115fff53b19f2796
Trevor Pfizenmaier <elektrikspark@gmail.com>
2022-10-12 00:57:37 -0500

    fix: posts mock data

--- components/layouts/sidebar/navbar-links-group/navbar-links-group.mocks.ts
+++ components/layouts/sidebar/navbar-links-group/navbar-links-group.mocks.ts
@@ -1,14 +1,11 @@
-import { IconCalendarStats } from '@tabler/icons';
-
 import { LinksGroupProps } from './navbar-links-group';
 
 const base: LinksGroupProps = {
-  label: 'Releases',
-  icon: IconCalendarStats,
-  links: [
-    { label: 'Upcoming releases', link: '/' },
-    { label: 'Previous releases', link: '/' },
-    { label: 'Releases schedule', link: '/' },
+  title: 'Releases',
+  posts: [
+    { title: 'Upcoming releases', slug: '/' },
+    { title: 'Previous releases', slug: '/' },
+    { title: 'Releases schedule', slug: '/' },
   ],
 };
 
