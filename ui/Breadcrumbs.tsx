881ea3568e3184917f837f8bde3fc2fc7eceaf4f
Trevor Pfizenmaier <elektrikspark@gmail.com>
2022-12-08 05:55:00 -0600

    fix: breadcrumbs a to next/link

--- ui/Breadcrumbs.tsx
+++ ui/Breadcrumbs.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { ChevronRightIcon, HomeIcon } from '@heroicons/react/20/solid';
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { Fragment } from 'react';
 
@@ -14,10 +15,10 @@ export default function Breadcrumbs() {
     <nav className="flex max-w-3xl" aria-label="Breadcrumb">
       <div className="flex items-center space-x-0 overflow-hidden">
         <div className="text-gray-400 hover:text-gray-500">
-          <a href={`/learn/${course}`}>
+          <Link href={`/learn/${course}`}>
             <HomeIcon className="h-5 w-5 flex-shrink-0" aria-hidden="true" />
             <span className="sr-only">Course Home</span>
-          </a>
+          </Link>
         </div>
         {pages &&
           pages.map((page, i, pagesArray) => {
@@ -49,7 +50,7 @@ export default function Breadcrumbs() {
                   className="h-5 w-5 flex-shrink-0 text-gray-400"
                   aria-hidden="true"
                 />
-                <a
+                <Link
                   href={`/learn/${course}/${pages[0]}`}
                   className="ml-0 min-w-[24px] truncate text-sm font-medium text-gray-400 hover:text-gray-700"
                   aria-current={undefined}
@@ -57,7 +58,7 @@ export default function Breadcrumbs() {
                   {page
                     .replace(/-/g, ' ')
                     .replace(/(^\w{1})|(\s+\w{1})/g, (letter) => letter.toUpperCase())}
-                </a>
+                </Link>
               </Fragment>
             );
           })}
