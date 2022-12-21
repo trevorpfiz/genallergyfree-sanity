d7a4b9e7a03e7f1a81726a71746d683e12909eeb
ElektrikSpark <elektrikspark@gmail.com>
2022-12-21 17:25:05 -0600

    fix: sitemap url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.VERCEL_URL || 'https://www.generationallergyfree.com/',
+  siteUrl: 'https://www.generationallergyfree.com/',
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
