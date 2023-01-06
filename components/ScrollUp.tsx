e3a6964250db94d663dd823cb6388a43288b79ec
ElektrikSpark <elektrikspark@gmail.com>
2023-01-06 05:09:38 -0600

    fix: setTimeout confirms, added id to html

--- components/ScrollUp.tsx
+++ components/ScrollUp.tsx
@@ -4,9 +4,7 @@ import { useEffect } from 'react';
 
 export default function ScrollUp() {
   useEffect(() => {
-    setTimeout(() => {
-      window.scrollTo(0, 0);
-    }, 1000);
+    document.getElementById('scroll')?.scrollTo(0, 0);
   }, []);
 
   return null;
